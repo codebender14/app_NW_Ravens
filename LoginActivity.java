72557ca64b1440d558f4675c154cc8116fa65009
Raghava Chowdary veeragandham <111784607+raghava249@users.noreply.github.com>
2023-03-03 22:20:58 -0600

    Created login Page
    Login page contains Email, Password, Login Button, forgot password and sign up.

---
+++ LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.nwravens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, NotificationHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
